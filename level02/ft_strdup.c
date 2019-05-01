1c98809381f89d051e27d793e0c9b5378c8bb090
Andrew Po <ensimgen@gmail.com>
2019-04-30 18:40:19 -0700

    Add files via upload

---
+++ level02/ft_strdup.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aponomar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/12 10:47:05 by aponomar          #+#    #+#             */
+/*   Updated: 2019/04/12 11:04:14 by aponomar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	char *s;
+	int	len;
+
+	len = 0;
+	while (src[len])
+		len++;
+	if (!(s = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	s[len] = '\0';
+	while (len >= 0)
+	{
+		s[len] = src[len];
+		len--;
+	}
+	return (s);
+}
---
+++ level02/swap_bits.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap_bits.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aponomar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/15 23:08:44 by aponomar          #+#    #+#             */
+/*   Updated: 2019/04/15 23:52:26 by aponomar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+unsigned char	swap_bits(unsigned char octet)
+{
+	return ((octet >> 4) | (octet << 4));
+}
+
+int	main(void)
+{
+	char c;
+
+	c = 't';
+	write(1, &c, 1);
+	c = swap_bits(c);
+	write(1, &c, 1);
+	return (0);
+}
