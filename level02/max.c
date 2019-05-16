892bf6090772768d273ee52344255c1d63b25280
Andrew Po <ensimgen@gmail.com>
2019-05-15 20:43:11 -0700

    Update max.c

--- level02/max.c
+++ level02/max.c
@@ -10,39 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <stdio.h>
+#include <unistd.h>
 
-
-int max(int* tab, unsigned int len)
+int        max(int* tab, unsigned int len)
 {
-	int max;
-	unsigned int i;
-
-	i = 0;
-	max = 0;
-	if (len > 0)
-	{
-		while (i < len)
-		{
-			if (tab[i] > max)
-				max = tab[i];
-			i++;
-			return (max);
-		}
-	}
-	return (0);
+    int i;
+    int max;
+    
+    i = 0;
+    max = 0;
+    if (len == 0)
+        return (0);
+    while (i < len)
+    {
+        if (tab[i] > max)
+            max = tab[i++];
+        i++;
+    }
+    return (max);  
 }
-
-int		main(void)//
-{
-	int	*tab;//
-
-	if (!(tab = (int*)malloc(sizeof(int) * 3)))//
-		return (0);//
-	tab[0] = 7;//
-	tab[1] = 66;//
-	tab[2] = 4;//
-	printf("%d", max(tab, 3));//
-	return (0);//
-}//
